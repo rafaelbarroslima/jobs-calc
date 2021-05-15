8bafe9f8908300c056fbecd9c8cfb6167cdd1044
rafaelbarroslimaa <rafaelbarroslimaa@gmail.com>
2021-05-14 22:45:59 -0300

    atualizado calculos de quanto o usuario quer ganhar no mes baseado nas horas trabalhadas, dias de folga, etc..

--- src/routes.js
+++ src/routes.js
@@ -3,84 +3,132 @@ const routes = express.Router();
 
 const views = __dirname + "/views/";
 
-const profile = {
-    name: "Rafael",
-    avatar: "https://avatars.githubusercontent.com/u/62842913?v=4",
-    "monthly-budget": 3000,
-    "days-per-week": 5,
-    "hours-per-day": 5,
-    "vacation-per-year": 4,
-    "value-hour": 14
-}
-
-const jobs = [
-    {
-        id: 1,
-        name: "Pizzaria Guloso",
-        "daily-hours": 2,
-        "total-hours": 1,
-        created_at: Date.now()
+const Profile = {
+    data: {
+        name: "Rafael",
+        avatar: "https://avatars.githubusercontent.com/u/62842913?v=4",
+        "monthly-budget": 3000,
+        "days-per-week": 5,
+        "hours-per-day": 5,
+        "vacation-per-year": 4,
+        "value-hour": 14
     },
-    {
-        id: 2,
-        name: "OneTwo Prokect",
-        "daily-hours": 3,
-        "total-hours": 47,
-        created_at: Date.now()
+
+    controllers: {
+        index(req, res) {
+            return res.render(views + "profile", { profile: Profile.data });
+        },
+
+        update(req, res) {
+            // req.body para pegar os dados
+            const data = req.body;
+
+            // Definir quantas semanas tem um ano: 52
+            const weeksPerYear = 52;
+
+            // Remover as semanas de férias do ano, para pegar quantas semanas tem 1 mês
+            const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12;
+
+            // Total de horas trabalhadas na semana
+            const weekTotalHours = data["hours-per-day"] * data["days-per-week"];
+
+            // Horas trabalhadas no mês
+            const monthlyTotalHours = weekTotalHours * weeksPerMonth;
+
+            // Qual será o valor da minha hora
+            const valueHour = data["monthly-budget"] / monthlyTotalHours;
+
+            Profile.data = {
+                ...Profile.data,
+                ...req.body,
+                "value-hour": valueHour
+            }
+
+            return res.redirect('/profile');
+        }
+
     }
-];
+};
 
-function remainingDays(job) {
-    const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed();
+const Job = {
+    data: [
+        {
+            id: 1,
+            name: "Pizzaria Guloso",
+            "daily-hours": 2,
+            "total-hours": 1,
+            created_at: Date.now()
+        },
+        {
+            id: 2,
+            name: "OneTwo Prokect",
+            "daily-hours": 3,
+            "total-hours": 47,
+            created_at: Date.now()
+        }
+    ],
 
-    const createdDate = new Date(job.created_at);
-    const dueDay = createdDate.getDate() + Number(remainingDays);
-    const dueDateInMs = createdDate.setDate(dueDay);
+    controllers: {
+        index(req, res) {
+            const updatedJobs = Job.data.map((job) => {
+            
+                const remaining = Job.services.remainingDays(job);
+                const status = remaining <= 0 ? 'done' : 'progress';
+        
+                return {
+                    ...job,
+                    remaining,
+                    status,
+                    budget: Profile.data["value-hour"] * job["total-hours"]
+                };
+            });
 
-    const timeDiffInMs = dueDateInMs - Date.now();
+            return res.render(views + "index", { jobs: updatedJobs });
+        },
 
-    const dayInMs = 1000 * 60 * 60 * 24;
-    const dayDiff = Math.floor(timeDiffInMs / dayInMs);
+        create(req, res) {
+            return res.render(views + "job");
+        },
 
-    return dayDiff;
-}
+        save(req,res) {
+            //req.body = { name: 'asas', 'daily-hours': '3', 'total-hours': '5' }
+            const lastId = Job.data[Job.data.length -1]?.id || 1;
 
-// request, response
-routes.get("/", (req, res) => {
+            Job.data.push({
+                id: lastId + 1,
+                name: req.body.name,
+                "daily-hours": req.body["daily-hours"],
+                "total-hours": req.body["total-hours"],
+                created_at: Date.now() // Atribuindo data de hoje
+            });
+        
+            return res.redirect("/");
+        }
+    },
 
-    const updatedJobs = jobs.map((job) => {
-    
-        const remaining = remainingDays(job);
-        const status = remaining <= 0 ? 'done' : 'progress';
+    services: {
+        remainingDays(job) {
+            const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed();
+        
+            const createdDate = new Date(job.created_at);
+            const dueDay = createdDate.getDate() + Number(remainingDays);
+            const dueDateInMs = createdDate.setDate(dueDay);
+        
+            const timeDiffInMs = dueDateInMs - Date.now();
+        
+            const dayInMs = 1000 * 60 * 60 * 24;
+            const dayDiff = Math.floor(timeDiffInMs / dayInMs);
+        
+            return dayDiff;
+        }
+    }
+};
 
-        return {
-            ...job,
-            remaining,
-            status,
-            budget: profile["value-hour"] * job["total-hours"]
-        };
-    });
-
-    return res.render(views + "index", { jobs: updatedJobs });
-});
-
-routes.get("/job", (req, res) => res.render(views + "job"));
-routes.post("/job", (req, res) => {
-    //req.body = { name: 'asas', 'daily-hours': '3', 'total-hours': '5' }
-    const lastId = jobs[jobs.length -1]?.id || 1;
-
-    jobs.push({
-        id: lastId + 1,
-        name: req.body.name,
-        "daily-hours": req.body["daily-hours"],
-        "total-hours": req.body["total-hours"],
-        created_at: Date.now() // Atribuindo data de hoje
-    });
-
-    return res.redirect("/");
-});
+routes.get("/", Job.controllers.index);
+routes.get("/job", Job.controllers.create);
+routes.post("/job", Job.controllers.save);
 routes.get("/job-edit", (req, res) => res.render(views + "job-edit"));
-routes.get("/profile", (req, res) => res.render(views + "profile", { profile }));
-
+routes.get("/profile", Profile.controllers.index);
+routes.post("/profile", Profile.controllers.update);
 
 module.exports = routes;
