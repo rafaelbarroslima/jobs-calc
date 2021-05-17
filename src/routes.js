9a48a597bcff09b2dd7d6c12fe1470e5a8338e00
rafaelbarroslimaa <rafaelbarroslimaa@gmail.com>
2021-05-17 13:05:55 -0300

    update routes, index

--- src/routes.js
+++ src/routes.js
@@ -79,7 +79,7 @@ const Job = {
                     ...job,
                     remaining,
                     status,
-                    budget: Profile.data["value-hour"] * job["total-hours"]
+                    budget: Job.services.calculateBudget(job, Profile.data["value-hour"])
                 };
             });
 
@@ -92,7 +92,7 @@ const Job = {
 
         save(req,res) {
             //req.body = { name: 'asas', 'daily-hours': '3', 'total-hours': '5' }
-            const lastId = Job.data[Job.data.length -1]?.id || 1;
+            const lastId = Job.data[Job.data.length -1]?.id || 0;
 
             Job.data.push({
                 id: lastId + 1,
@@ -102,6 +102,50 @@ const Job = {
                 created_at: Date.now() // Atribuindo data de hoje
             });
         
+            return res.redirect("/");
+        },
+
+        show(req, res) {
+
+            const jobId = req.params.id;
+            const job = Job.data.find(job => Number(job.id) === Number(jobId));
+            if(!job) {
+                return res.send("Job not found!");
+            }
+            job.budget = Job.services.calculateBudget(job, Profile.data["value-hour"]);
+            return res.render(views + "job-edit", { job });
+        },
+
+        update(req, res) {
+            const jobId = req.params.id;
+            const job = Job.data.find(job => Number(job.id) === Number(jobId));
+
+            if (!job) {
+                return res.send("Job not found!");
+            };
+
+            const updatedJob = {
+                ...job,
+                name: req.body.name,
+                "total-hours": req.body["total-hours"],
+                "daily-hours": req.body["daily-hours"]
+            };
+
+            Job.data = Job.data.map(job => {
+                if(Number(job.id) === Number(jobId)) {
+                    job = updatedJob;
+                };
+                return job;
+            });
+
+            res.redirect('/job/' + jobId);
+        },
+
+        delete(req, res) {
+            const jobId = req.params.id;
+
+            Job.data = Job.data.filter(job => Number(job.id) !== Number(jobId));
+
             return res.redirect("/");
         }
     },
@@ -120,14 +164,18 @@ const Job = {
             const dayDiff = Math.floor(timeDiffInMs / dayInMs);
         
             return dayDiff;
-        }
+        },
+
+        calculateBudget: (job, valueHour) => valueHour * job["total-hours"]
     }
 };
 
 routes.get("/", Job.controllers.index);
 routes.get("/job", Job.controllers.create);
 routes.post("/job", Job.controllers.save);
-routes.get("/job-edit", (req, res) => res.render(views + "job-edit"));
+routes.get("/job/:id", Job.controllers.show);
+routes.post("/job/:id", Job.controllers.update);
+routes.post("/job/delete/:id", Job.controllers.delete);
 routes.get("/profile", Profile.controllers.index);
 routes.post("/profile", Profile.controllers.update);
 
